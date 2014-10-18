18035c1002b91e3dbdb066f2863f8cae6911795e
David Stosik <david.stosik+git-noreply@gmail.com>
2014-10-18 17:48:25 +0900

    Usew GitHub square account.

--- config/navigations/user_navigation.rb
+++ config/navigations/user_navigation.rb
@@ -1,7 +1,7 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav navbar-nav navbar-right'
-    primary.item :login, t('menu.user.login'), auth_path(:github), unless: proc { current_user }
+    primary.item :login, t('menu.user.login'), auth_path(:github), icon: 'fa fa-github-square', unless: proc { current_user }
     primary.item :account, user_label(current_user), if: proc { current_user } do |account|
       account.item :logout, t('menu.user.logout'), logout_path
     end
