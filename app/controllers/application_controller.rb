16c2507c3e9e9e74c80d8cef021162fa95f83a8e
David Stosik <david.stosik+git-noreply@gmail.com>
2014-10-19 11:09:24 +0900

    Force HTTPS to a defined CANONICAL_HOST.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  def self.ssl_options
+    options = {if: :ssl_configured?}
+    options[:host] = ENV['CANONICAL_HOST'] if ENV['CANONICAL_HOST']
+    options
+  end
+  force_ssl ssl_options
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -9,6 +16,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 
 end
