78dbd1b5d7b9827fc2c630c3f371c6c9738f5df0
David Stosik <david.stosik+git-noreply@gmail.com>
2014-10-19 19:41:06 +0900

    Fix some commit cloning code.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,12 +3,20 @@ class Commit < GitObject
     @tree ||= Tree.new repository, data.commit.tree.sha
   end
 
-  def clone dest_repo
+  def clone dest_repo, time=nil
     existing_clone = existing_clone(dest_repo)
     return existing_clone if existing_clone
 
+    parents = nil #TODO: find them, get a clone
     new_tree = tree.clone dest_repo
-    new_commit = dest_repo.create 'commit', data.commit.message, new_tree.sha #TODO parents
+
+    options = Hash[[:author, :commiter].map do |key|
+      user_data = data.commit.send(key).to_h
+      user_data[:date] = time if time
+      [key, user_data]
+    end
+
+    new_commit = dest_repo.create 'commit', data.commit.message, new_tree.sha, parents, options
     map_clone new_commit
     new_commit
   end
