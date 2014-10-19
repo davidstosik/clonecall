e1e47b1cbe20e8a1e6858fab4d4731d4bd7999d4
David Stosik <david.stosik+git-noreply@gmail.com>
2014-10-20 03:49:28 +0900

    Validate and create CloneCallJob.

--- app/controllers/clone_call_jobs_controller.rb
+++ app/controllers/clone_call_jobs_controller.rb
@@ -1,15 +1,33 @@
 class CloneCallJobsController < ApplicationController
   before_filter :authorize
 
-  def new
-    @clone_call_job = CloneCallJob.new user: current_user
-    @repo_options = current_user.repositories.map do |name, repo|
-      [name, name]
-    end
-  end
-
   def index
     @clone_calls = []
+  end
+
+  def new
+    @clone_call_job = scope.new start_at: 45.hours.ago.utc, end_at: Time.now.utc, branch: 'master'
+    @repo_options = current_user.repositories.keys
+  end
+
+  def create
+    @clone_call_job = scope.new params.require(:clone_call_job)
+      .permit %w(src_repo branch dst_repo start_at end_at)
+
+    if @clone_call_job.save
+      #flash! :success
+      redirect_to clone_call_jobs_path
+    else
+      #flash! :error
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def scope
+    current_user.clone_call_jobs
   end
   
 end
--- app/models/clone_call_job.rb
+++ app/models/clone_call_job.rb
@@ -1,3 +1,19 @@
 class CloneCallJob < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :src_repo, :dst_repo, :start_at, :end_at, :user
+  validate :dates_are_valid
+
+  private
+
+  def dates_are_valid
+    if end_at < start_at
+     errors.add(:start_at, "cannot be after the end date")
+    end
+
+    if end_at > Time.now
+     errors.add(:end_at, "cannot be after now")
+    end
+  end
+
 end
