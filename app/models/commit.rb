024b916036f8dc5b0e2589e0136072e1bd29813c
David Stosik <david.stosik+git-noreply@gmail.com>
2014-10-19 18:28:21 +0900

    Introduce commit cloning logic. Way to go!

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,8 +3,9 @@ class Commit < GitObject
     @tree ||= Tree.new repository, data.commit.tree.sha
   end
 
-  def clone dest_repository, commit_at
-
+  def clone dest_repo
+    new_tree = tree.clone dest_repo
+    new_commit = dest_repo.create 'commit', data.commit.message, new_tree.sha #TODO parents
   end
 
 end
